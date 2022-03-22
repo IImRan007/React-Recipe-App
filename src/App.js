647b8346427e2d9decf1ea4c385e1141c4eed502
Mohd Imran <mohd78696321@gmail.com>
2022-03-22 16:14:34 +0530

    added search page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import Search from "./components/Search";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
